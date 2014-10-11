4b46a9198fcb87b9f8c7d96e2d9bf28ef712acbc
Sterling Houghton <aussielb@gmail.com>
2014-10-11 08:39:08 -0700

    add restaurant_id to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141011034418) do
+ActiveRecord::Schema.define(version: 20141011151810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20141011034418) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "restaurant_id"
   end
 
   create_table "users", force: true do |t|
