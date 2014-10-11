8b265b16150962d47c6b8591ebf103845e76ccbb
Sterling Houghton <aussielb@gmail.com>
2014-10-11 08:40:49 -0700

    set @review.restaurant_id to restaurant.id

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:edit, :update, :destroy]
+  before_action :set_restaurant
   before_action :authenticate_user!
 
   # GET /reviews/new
@@ -15,6 +16,7 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
+    @review.restaurant_id = @restaurant.id
     if @review.save
       redirect_to root_path, notice: 'Review was successfully created.'
     else
@@ -43,6 +45,9 @@ class ReviewsController < ApplicationController
       @review = Review.find(params[:id])
     end
 
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:restaurant_id])
+    end
     # Only allow a trusted parameter "white list" through.
     def review_params
       params.require(:review).permit(:rating, :comment)
