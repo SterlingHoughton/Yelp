8c848ec03dadfa8d0ba7922243fb7e5b8db71045
Sterling Houghton <aussielb@gmail.com>
2014-10-24 09:31:32 -0700

    fix bug in model:restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,12 +4,12 @@ class Restaurant < ActiveRecord::Base
   has_many :reviews
 
   searchkick
-  
+
   validates :name, :address, :phone, :website, :image, presence: true
   validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
     message: "must be in the format (123) 456-7890" }
   validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
     message: "must start with http:// or https://" }
-  validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A_Z]{2} \d{5}\z/,
-    message: "must be in the format 208 Fifth Avenue, Portland, OR 97204" }
+  # validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A_Z]{2} \d{5}\z/,
+  #   message: "must be in the format 208 Fifth Avenue, Portland, OR 97204" }
 end
